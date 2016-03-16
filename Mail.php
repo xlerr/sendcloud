33f32dba115dbca1b93d8865af01fb1b6d34e9b7
pxl <997190793@qq.com>
2016-03-16 18:22:00 +0800

    整理

--- Mail.php
+++ Mail.php
@@ -11,10 +11,10 @@ use Yii;
  * 'components' => []
  *　    'sendCloud' => [
  *　        'class'    => 'xlerr\sendcloud\Mail',
- * 	       'user'     => 'user',
- * 	       'key'      => 'key',
- * 	       'from'     => 'aaa@qq.com',
- * 	       'fromname' => 'from name',
+ *         'user'     => 'user',
+ *         'key'      => 'key',
+ *         'from'     => 'aaa@qq.com',
+ *         'fromname' => 'from name',
  *     ],
  * ],
  * ~~~~~~~~
@@ -23,136 +23,136 @@ use Yii;
  * 单次发送实例 
  * ~~~~~~~~
  * (new Mail)->to('alskdjf@163.com')->compose('tempateName', 'subject', [
- * 	'key1' => ['param1'],
- * 	'key2' => ['param2'],
+ *  'key1' => ['param1'],
+ *  'key2' => ['param2'],
  * ]);
  * ~~~~~~~~
  *
  * 群发实例
  * ~~~~~~~~
  * (new Mail)->to(['alskdjf@163.com', 'aaaaa@163.com'])->compose('tempateName', 'subject', [
- * 	'key1' => ['param1', 'param11'],
- * 	'key2' => ['param2', 'param22'],
+ *  'key1' => ['param1', 'param11'],
+ *  'key2' => ['param2', 'param22'],
  * ]);
  * ~~~~~~~~
  */
 class Mail extends Request
 {
-	/**
-	 * 邮件主题
-	 * @var string
-	 */
-	public $subject;
+    /**
+     * 邮件主题
+     * @var string
+     */
+    public $subject;
 
-	/**
-	 * 邮件模板名称
-	 * @var string
-	 */
-	public $template;
+    /**
+     * 邮件模板名称
+     * @var string
+     */
+    public $template;
 
-	/**
-	 * 收件人列表
-	 * @var array
-	 */
-	private $_to;
+    /**
+     * 收件人列表
+     * @var array
+     */
+    private $_to;
 
-	/**
-	 * 邮件模板中的占位符替换值(不同模板有不同的占位符)
-	 * @var array
-	 */
-	private $_sub;
+    /**
+     * 邮件模板中的占位符替换值(不同模板有不同的占位符)
+     * @var array
+     */
+    private $_sub;
 
-	/**
-	 * 发送者
-	 * @var string
-	 */
-	public $from = '';
+    /**
+     * 发送者
+     * @var string
+     */
+    public $from = '';
 
-	/**
-	 * 发送者名字
-	 * @var string
-	 */
-	public $fromname = '';
+    /**
+     * 发送者名字
+     * @var string
+     */
+    public $fromname = '';
 
-	/**
-	 * 调用该方法发送邮件
-	 * @return json
-	 */
-	public function send()
-	{
-		if ($this->beforeSend() !== true) {
-			return false;
-		}
+    /**
+     * 调用该方法发送邮件
+     * @return json
+     */
+    public function send()
+    {
+        if ($this->beforeSend() !== true) {
+            return false;
+        }
 
-		return $this->request([
-			'from'                 => $this->from,
-			'fromname'             => $this->fromname,
-			'template_invoke_name' => $this->template,
-			'subject'              => $this->subject,
-			'resp_email_id'        => 'true',
-			'substitution_vars'    => json_encode([
-				'to'  => $this->_to,
-				'sub' => $this->_sub,
-			]),
-		]);
-	}
+        return $this->request([
+            'from'                 => $this->from,
+            'fromname'             => $this->fromname,
+            'template_invoke_name' => $this->template,
+            'subject'              => $this->subject,
+            'resp_email_id'        => 'true',
+            'substitution_vars'    => json_encode([
+                'to'  => $this->_to,
+                'sub' => $this->_sub,
+            ]),
+        ]);
+    }
 
-	/**
-	 * 发送邮件之前需要验证相关数据
-	 * 
-	 * @return return boolean
-	 */
-	public function beforeSend()
-	{
-		if (empty($this->subject)) {
-			$this->setError('subject cannot be null');
-			return false;
-		}
-		if (empty($this->template)) {
-			$this->setError('template cannot be null');
-			return false;
-		}
+    /**
+     * 发送邮件之前需要验证相关数据
+     * 
+     * @return return boolean
+     */
+    public function beforeSend()
+    {
+        if (empty($this->subject)) {
+            $this->setError('subject cannot be null');
+            return false;
+        }
+        if (empty($this->template)) {
+            $this->setError('template cannot be null');
+            return false;
+        }
 
-		// 获取收件人数量
-		$recipientCount = count($this->_to);
+        // 获取收件人数量
+        $recipientCount = count($this->_to);
 
-		if ($recipientCount < 1) {
-			$this->setError('Please set the recipient');
-			return false;
-		}
-		return true;
-	}
+        if ($recipientCount < 1) {
+            $this->setError('Please set the recipient');
+            return false;
+        }
+        return true;
+    }
 
-	public function To($to)
-	{
-		$this->_to = (array) $to;
+    public function To($to)
+    {
+        $this->_to = (array) $to;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getTo()
-	{
-		return $this->_to;
-	}
+    public function getTo()
+    {
+        return $this->_to;
+    }
 
-	public function Sub($sub)
-	{
-		$this->_sub = $sub;
+    public function Sub($sub)
+    {
+        $this->_sub = $sub;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getSub()
-	{
-		return $this->_sub;
-	}
+    public function getSub()
+    {
+        return $this->_sub;
+    }
 
-	public function compose($template, $subject, $sub)
-	{
-		$this->template = $template;
-		$this->subject = $subject;
-		$this->_sub = $sub;
+    public function compose($template, $subject, $sub)
+    {
+        $this->template = $template;
+        $this->subject = $subject;
+        $this->_sub = $sub;
 
-		return $this;
-	}
+        return $this;
+    }
 }
--- Request.php
+++ Request.php
@@ -5,118 +5,118 @@ use Yii;
 
 class Request
 {
-	/**
-	 * 调试开关
-	 * @var boolean
-	 */
-	public $debug = false;
+    /**
+     * 调试开关
+     * @var boolean
+     */
+    public $debug = false;
 
-	/**
-	 * 请求地址（一般情况不需要修改）
-	 * @var string
-	 */
-	public $url = 'https://sendcloud.sohu.com/webapi/mail.send_template.json';
+    /**
+     * 请求地址（一般情况不需要修改）
+     * @var string
+     */
+    public $url = 'https://sendcloud.sohu.com/webapi/mail.send_template.json';
 
-	/**
-	 * 授权账号
-	 * @var string
-	 */
-	public $user;
+    /**
+     * 授权账号
+     * @var string
+     */
+    public $user;
 
-	/**
-	 * 授权 key
-	 * @var string
-	 */
-	public $key;
+    /**
+     * 授权 key
+     * @var string
+     */
+    public $key;
 
-	/**
-	 * 异常信息
-	 * @var string
-	 */
-	private $error;
+    /**
+     * 异常信息
+     * @var string
+     */
+    private $error;
 
-	/**
-	 * 缓存POST数据
-	 * @var array
-	 */
-	private $_params;
+    /**
+     * 缓存POST数据
+     * @var array
+     */
+    private $_params;
 
-	public function request($params = [])
-	{
-		if ($this->beforeRequest() === false) {
-			return false;
-		}
+    public function request($params = [])
+    {
+        if ($this->beforeRequest() === false) {
+            return false;
+        }
 
-		$this->_params = (array) $params;
+        $this->_params = (array) $params;
 
-		$context  = stream_context_create([
-			'http' => [
-				'method'  => 'POST',
-				'header'  => 'Content-Type: application/x-www-form-urlencoded',
-				'content' => http_build_query(array_merge($this->_params, [
-					'api_user' => $this->user,
-					'api_key'  => $this->key,
-				])),
-			],
-		]);
+        $context  = stream_context_create([
+            'http' => [
+                'method'  => 'POST',
+                'header'  => 'Content-Type: application/x-www-form-urlencoded',
+                'content' => http_build_query(array_merge($this->_params, [
+                    'api_user' => $this->user,
+                    'api_key'  => $this->key,
+                ])),
+            ],
+        ]);
 
-		$result = file_get_contents($this->url, FILE_TEXT, $context);
+        $result = file_get_contents($this->url, FILE_TEXT, $context);
 
-		return $this->afterRequest($result);
-	}
+        return $this->afterRequest($result);
+    }
 
-	public function beforeRequest()
-	{
-		if (!$this->url) {
-			$this->setError('Cannot request a null URL!');
-			return false;
-		}
-		if (!$this->user) {
-			$this->setError('Authorization ID cannot be empty!');
-			return false;
-		}
-		if (!$this->key) {
-			$this->setError('Authorization token cannot be empty!');
-			return false;
-		}
-		return true;
-	}
+    public function beforeRequest()
+    {
+        if (!$this->url) {
+            $this->setError('Cannot request a null URL!');
+            return false;
+        }
+        if (!$this->user) {
+            $this->setError('Authorization ID cannot be empty!');
+            return false;
+        }
+        if (!$this->key) {
+            $this->setError('Authorization token cannot be empty!');
+            return false;
+        }
+        return true;
+    }
 
-	public function afterRequest($result)
-	{
-		$info = json_decode($result, true);
-		if ($this->debug === true) {
-			Yii::trace([
-				'params'      => $this->_params,
-				'result'      => $result,
-				'result_json' => $info,
-			], __CLASS__);
-		}
-		if ($info === null) {
-			$this->setError('Abnormal results');
-			return false;
-		}
-		if ($info['message'] === 'error') {
-			$this->setError($info['errors']);
-			return false;
-		}
-		if ($info['message'] !== 'success') {
-			$this->setError('Unknown error');
-			return false;
-		}
-		return $info;
-	}
+    public function afterRequest($result)
+    {
+        $info = json_decode($result, true);
+        if ($this->debug === true) {
+            Yii::trace([
+                'params'      => $this->_params,
+                'result'      => $result,
+                'result_json' => $info,
+            ], __CLASS__);
+        }
+        if ($info === null) {
+            $this->setError('Abnormal results');
+            return false;
+        }
+        if ($info['message'] === 'error') {
+            $this->setError($info['errors']);
+            return false;
+        }
+        if ($info['message'] !== 'success') {
+            $this->setError('Unknown error');
+            return false;
+        }
+        return $info;
+    }
 
-	public function setError($error)
-	{
-		if ($this->debug) {
-			Yii::error($error, __CLASS__);
-		}
-		$this->error = $error;
-	}
+    public function setError($error)
+    {
+        if ($this->debug) {
+            Yii::error($error, __CLASS__);
+        }
+        $this->error = $error;
+    }
 
-	public function getError()
-	{
-		return $this->error;
-	}
+    public function getError()
+    {
+        return $this->error;
+    }
 }
